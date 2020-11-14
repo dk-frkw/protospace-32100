bb9cc59f4b368ce2288832434bb2c18f0fa5aba6
dk-frkw <sp.daiki98@gmail.com>
2020-11-15 03:25:01 +0900

    Userモデルのバリデーション設定

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,9 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+  
+  validatable :name, presence: true
+  validatable :profile, presence: true
+  validatable :occupation, presence: true
+  validatable :position, presence: true
 end
