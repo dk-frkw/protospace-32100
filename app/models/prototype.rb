b0d099efab61725e0d5c446f5dbffcb5c842707a
dk-frkw <sp.daiki98@gmail.com>
2020-11-16 17:12:27 +0900

    バリデーションを設定

--- app/models/prototype.rb
+++ app/models/prototype.rb
@@ -1,5 +1,10 @@
 class Prototype < ApplicationRecord
-  belongs_to :user
   
+　validates :title, presence: true
+  validates :catch_copy, presence: true
+  validates :concept, presence: true
+  validates :image, presence: true
+  
+  belongs_to :user
   has_one_attached :image
 end
