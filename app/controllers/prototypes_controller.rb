58d16fe3363a7cac93ddf79cfb09b1ee8af0d26a
dk-frkw <sp.daiki98@gmail.com>
2020-11-20 21:57:48 +0900

    追加修正3

--- app/controllers/prototypes_controller.rb
+++ app/controllers/prototypes_controller.rb
@@ -1,7 +1,6 @@
 class PrototypesController < ApplicationController
   before_action :authenticate_user!, only: [:new, :create, :destroy, :edit, :update]
-  before_action :move_to_index, except: [:index, :show, :new, :create]
-
+  
   def index
     @prototypes = Prototype.all
   end
@@ -15,8 +14,7 @@ class PrototypesController < ApplicationController
      if @prototype.save
        redirect_to root_path
      else
-       @prototype = prototypes.includes(:user)
-       render ：new
+       render :new
      end
   end
 
@@ -28,7 +26,9 @@ class PrototypesController < ApplicationController
 
   def edit
     @prototype = Prototype.find(params[:id])
-    
+    unless current_user.id == @prototype.user_id
+      redirect_to action: :index
+     end
   end
 
   def update
@@ -52,12 +52,4 @@ class PrototypesController < ApplicationController
   def prototype_params
     params.require(:prototype).permit(:title, :catch_copy, :concept, :image).merge(user_id: current_user.id)
   end
-
-  def move_to_index
-    @prototype = Prototype.find(params[:id])
-    unless current_user.id == @prototype.user_id
-      redirect_to action: :index
-    end
-  end
-
 end
